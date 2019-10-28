53d9837035298eaec37bf6e5b93207c5f1ba4b1a
Florian Krull <flo.krull@gmail.com>
2019-10-28 12:18:27 +0100

    ask for name on profile delete

--- lib/scselectcfg.dart
+++ lib/scselectcfg.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:mirrortask/helper.dart';
 import 'package:mirrortask/scsetscrw.dart';
 import 'package:mirrortask/settings.dart';
@@ -51,12 +52,16 @@ class _SelectConfigScreenState extends State<SelectConfigScreen> {
             children: <Widget>[
               Expanded(
                 flex: 1,
-                child: Center(
-                  child: Text("Select a profile", style: Theme.of(context).textTheme.headline,)
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Padding(
+                    padding: EdgeInsets.only(bottom: 20),
+                    child: Text("Select a profile",)
+                  )
                 )
               ),
               Expanded(
-                flex: 5,
+                flex: 4,
                 child: Center(
                   child: FractionallySizedBox(
                     widthFactor: 2/3,
@@ -68,7 +73,7 @@ class _SelectConfigScreenState extends State<SelectConfigScreen> {
                         final List<int> bytes = md5.convert(utf8.encode(projName)).bytes.sublist(0, 4);
                         final int sum = bytes.fold(0, (p, c) => (p + c));
                         return Dismissible(
-                          confirmDismiss: _confirmDelete,
+                          confirmDismiss: _getConfirmDelete(projName),
                           onDismissed: (e) async {
                             await LcSettings().delete(projName);
                             snapshot.data.remove(projName);
@@ -109,29 +114,31 @@ class _SelectConfigScreenState extends State<SelectConfigScreen> {
   }
 
 
-  Future<bool> _confirmDelete(DismissDirection direction) async {
-    final bool res = await showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return CupertinoAlertDialog(
-          title: const Text("Are you sure you wish to delete this item?"),
-          actions: [
-            CupertinoDialogAction(
-              isDefaultAction: false, 
-              child: Text("Delete"),
-              isDestructiveAction: true,
-              onPressed: () => Navigator.of(context).pop(true),
-            ),
-            CupertinoDialogAction(
-              isDefaultAction: true, 
-              child: Text("Back"),
-              isDestructiveAction: false,
-              onPressed: () => Navigator.of(context).pop(false),
-            ),
-          ]
-        );
-      },
-    );
-    return res;
+  Function _getConfirmDelete(String projName) {
+    return (DismissDirection direction) async {
+      final bool res = await showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return CupertinoAlertDialog(
+            title: Text("Are you sure you wish to delete profile '$projName'?"),
+            actions: [
+              CupertinoDialogAction(
+                isDefaultAction: false, 
+                child: Text("Delete"),
+                isDestructiveAction: true,
+                onPressed: () => Navigator.of(context).pop(true),
+              ),
+              CupertinoDialogAction(
+                isDefaultAction: true, 
+                child: Text("Back"),
+                isDestructiveAction: false,
+                onPressed: () => Navigator.of(context).pop(false),
+              ),
+            ]
+          );
+        },
+      );
+      return res;
+    };
   }
 }
--- lib/scstart.dart
+++ lib/scstart.dart
@@ -20,74 +20,87 @@ class StartScreen extends StatelessWidget {
           )
         );
       },
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Expanded(
-              child: Center(child: Text("Profile: ${LcSettings().activeConfigName}")),
-              flex: 1,
-            ),
-            CupertinoButton(
-              child: Text("Change profile"),
-              onPressed: () {
-                Navigator.of(context).pushReplacement(
-                  MaterialPageRoute(
-                    builder: (context) => SelectConfigScreen(),
-                  )
-                );
-              }
-            ),
-            CupertinoButton(
-              child: Text("Settings"),
-              onPressed: () {
-                showDialog(
-                  context: context,
-                  builder: (BuildContext context) => CupertinoAlertDialog(
-                    title: Text("Warning!"),
-                    content: Text("Changes in the settings might make new results uncomparable with previous ones."),
-                    actions: [
-                      CupertinoDialogAction(
-                        isDefaultAction: false, 
-                        child: Text("Back"),
-                        onPressed: () async {
-                          Navigator.of(context).pop();
-                        },
-                      ),
-                      CupertinoDialogAction(
-                        isDefaultAction: true, 
-                        child: Text("Ok"),
-                        isDestructiveAction: false,
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                          Navigator.of(context).push(
-                            MaterialPageRoute(
-                              builder: (context) => SettingsScreen(),
-                            )
-                          );
-                        },
-                      ),
-                    ],
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: EdgeInsets.only(bottom: 20),
+                child: Text("Profile: ${LcSettings().activeConfigName}"),
+              )
+            )
+          ),
+          Expanded(
+            child: Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  CupertinoButton(
+                    child: Text("Change profile"),
+                    onPressed: () {
+                      Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(
+                          builder: (context) => SelectConfigScreen(),
+                        )
+                      );
+                    }
                   ),
-                );
-              },
+                  CupertinoButton(
+                    child: Text("Settings"),
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) => CupertinoAlertDialog(
+                          title: Text("Warning!"),
+                          content: Text("Changes in the settings might make new results uncomparable with previous ones."),
+                          actions: [
+                            CupertinoDialogAction(
+                              isDefaultAction: false, 
+                              child: Text("Back"),
+                              onPressed: () async {
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                            CupertinoDialogAction(
+                              isDefaultAction: true, 
+                              child: Text("Ok"),
+                              isDestructiveAction: false,
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                                Navigator.of(context).push(
+                                  MaterialPageRoute(
+                                    builder: (context) => SettingsScreen(),
+                                  )
+                                );
+                              },
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                  CupertinoButton(
+                    child: Text("Start experiment"),
+                    onPressed: () {
+                      Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(
+                          builder: (context) => GetIdScreen( screenWidth: MediaQuery.of(context).size.width ),
+                        )
+                      );
+                    }
+                  ),
+                ],
+              ),
             ),
-            CupertinoButton(
-              child: Text("Start experiment"),
-              onPressed: () {
-                Navigator.of(context).pushReplacement(
-                  MaterialPageRoute(
-                    builder: (context) => GetIdScreen( screenWidth: MediaQuery.of(context).size.width ),
-                  )
-                );
-              }
-            ),
-            Expanded(
-              child: divx_0,
-              flex: 1,
-            ),
-          ],
-        )
+            flex: 4,
+          ),
+          Expanded(
+            child: divx_0,
+            flex: 1,
+          ),
+        ],
       )
     );
   }
